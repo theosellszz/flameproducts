79694e6b8fe66abef81d1aeeb9c1027e98b000dc
theosellszz <ted914063@gmail.com>
2025-05-21 17:03:21 -0400

    Delete App.jsx

--- App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to The Resell Store</h1>
-    </div>
-  )
-}
